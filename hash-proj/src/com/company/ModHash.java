6b5968dd77c68ad285b49f8a293e8441eb6189f0
noac7 <nc7@mail.tau.ac.il>
2022-05-15 14:22:13 +0300

    + ModHash

--- hash-proj/src/com/company/ModHash.java
+++ hash-proj/src/com/company/ModHash.java
@@ -1,14 +1,24 @@
 package com.company;
+import java.util.Random;
+import java.util.function.Function;
 
 public class ModHash {
+
+	private final Function<Long, Integer> func;
+
+	private ModHash(long a, long b, int m, long p){
+		this.func = (Long x)-> (int)(((a*x + b) % p) % m);
+	}
 	
 	public static ModHash GetFunc(int m, long p){
-		// TODO implement random choice of mod function from family
-		return null;
+		Random r = new Random();
+		long a = (r.nextLong()  % (p - 1)) + 1;
+		long b = (r.nextLong()  % p);
+		return new ModHash(a, b, m, p);
+
 	}
 	
 	public int Hash(long key) {
-		// TODO implement hash function
-		return 0;
+		return this.func.apply(key);
 	}
 }
