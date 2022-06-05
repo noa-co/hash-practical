3376de31722e70b1b45781a3689ab3c1f595ef54
noac7 <nc7@mail.tau.ac.il>
2022-06-05 21:23:42 +0300

    ~ changes hk2 to be random as requested.

--- hash-proj/src/com/company/DoubleHashTable.java
+++ hash-proj/src/com/company/DoubleHashTable.java
@@ -4,16 +4,14 @@ public class DoubleHashTable extends OAHashTable {
 	protected ModHash modHash2;
 	public DoubleHashTable(int m, long p) {
 		super(m, p);
-		this.modHash2 = ModHash.GetFunc(m, p);
+		this.modHash2 = ModHash.GetFunc(m-1, p);
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
 		long hk = this.modHash.Hash(x);
-		long hk2 = this.modHash2.Hash(x);
-		if (hk2 == 0){
-			hk2 = 1;
-		}
+		long hk2 = this.modHash2.Hash(x) + 1;
+
 		long res =  ((hk + (i * hk2)) );
 		return Math.floorMod((int) res , this.m);
 	}
