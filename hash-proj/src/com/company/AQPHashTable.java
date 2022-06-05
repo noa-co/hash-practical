8082b1e1fe099fa491f9e46ab6cd6261de265d4e
noac7 <nc7@mail.tau.ac.il>
2022-06-05 17:55:20 +0300

    Made AQP better

--- hash-proj/src/com/company/AQPHashTable.java
+++ hash-proj/src/com/company/AQPHashTable.java
@@ -9,7 +9,7 @@ public class AQPHashTable extends OAHashTable {
 	@Override
 	public int Hash(long x, int i) {
 		int hk = this.modHash.Hash(x);
-		int sign = (int) Math.pow(-1,(i));
+		int sign = i%2==0? 1 : -1; // Same as calculating -1 times i
 		return (Math.floorMod(Math.floorMod(hk + (sign)*(i*i), m) +this.m,this.m));
 	}
 }
